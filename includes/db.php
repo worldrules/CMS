c226c983117de21b787e36eca50a1e8faf3730a4
LEONARDO NUNES CARVALHO <33266800+worldrules@users.noreply.github.com>
2018-07-24 14:53:32 -0300

    Update db.php

--- includes/db.php
+++ includes/db.php
@@ -1,9 +1,9 @@
 <?php
 ob_start();
 $db['db_host'] = "us-cdbr-iron-east-04.cleardb.net";
-$db['db_user'] = "bb4d3eb9810e2e";
-$db['db_pass'] = "ca1732ea";
-$db['db_name'] = "heroku_c93c337c3424fd4";
+$db['db_user'] = "b27ea381e26bf4";
+$db['db_pass'] = "a7ffe6e3";
+$db['db_name'] = "heroku_024874c22c7ed87";
 
 
 foreach($db as $key => $value) {
