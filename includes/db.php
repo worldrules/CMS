39ca7bdc32dd60869a72e0f5f2c99bb53535431c
LEONARDO NUNES CARVALHO <33266800+worldrules@users.noreply.github.com>
2018-07-24 03:22:16 -0300

    Update db.php

--- includes/db.php
+++ includes/db.php
@@ -3,7 +3,7 @@ ob_start();
 $db['db_host'] = "mysql://bb4d3eb9810e2e:ca1732ea@us-cdbr-iron-east-04";
 $db['db_user'] = "bb4d3eb9810e2e";
 $db['db_pass'] = "ca1732ea";
-$db['db_name'] = "cms";
+$db['db_name'] = "heroku_c93c337c3424fd4";
 
 
 foreach($db as $key => $value) {
